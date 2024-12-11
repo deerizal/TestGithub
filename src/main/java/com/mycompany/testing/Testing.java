90a45bcf401e88bab38a422f4c1a1e0a303ec23a
deerizal <23001814@siswa.um.edu.my>
2024-12-11 13:17:28 +0800

    4d3pu

---
+++ src/main/java/com/mycompany/testing/Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing;
+
+/**
+ *
+ * @author 4d3pu
+ */
+public class Testing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
